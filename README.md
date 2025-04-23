43f8b228dceab0a72a9d4870c885ce3e69e71399
Maekiz <manoa039@osloskolen.no>
2025-04-23 21:26:57 +0200

    øvre sppe er sat på velocity.y og fart i horisontal rettning

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ src -> client -> java -> kurs.kursmod -> mixin.client -> FlyBoatMixin
 
 
 ## Lag din egen Minecraft Hacked Client fra scratch
-- Gå inn på linken: 
+- Gå inn på linken: https://fabricmc.net/develop/template/
 - Fyll inn skjemaet (Ikke bry deg om advanced settings)
 - Trykk på create zip folder
 - Åpne .zip filen
 - Åpne den ukomprimerte filen i IntelliJ (eller annen code editor)
-- 
+- Naviger deg fra til src/client/
--- src/client/java/kurs/kursmod/mixin/client/FlyBoatMixin.java
+++ src/client/java/kurs/kursmod/mixin/client/FlyBoatMixin.java
@@ -2,15 +2,21 @@ package kurs.kursmod.mixin.client;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.Vec3d;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class FlyBoatMixin {
-    private double VELOCITY_BUMP = 0.1;
-    private double SPEED = 1.1;
+    private static final Logger LOGGER = LoggerFactory.getLogger("Maekiz");
+    private static final double VELOCITY_BUMP = 0.09;
+    private static final double MAX_SPEED = 1.5;
+    private static final double MAX_VELY = 1.3;
+    private static final double SPEED = 1.1;
 
     @Inject(at = @At("HEAD"), method = "tick")
     private void init(CallbackInfo info) {
@@ -21,7 +27,8 @@ public class FlyBoatMixin {
         if (player == null){
             return;
         }
-        // Sjekke etter båt
+
+        //sjekker etter båt
 
         var vehicle = player.getVehicle();
 
@@ -29,17 +36,31 @@ public class FlyBoatMixin {
             return;
         }
 
-        // få vektorer og sett ny vektor
+        // sjekk etter jumpkey/space
+        Vec3d current = vehicle.getVelocity();
+        var velX = current.x * SPEED;
+        var velZ = current.z * SPEED;
+
+        double horizontalLen = Math.sqrt(velX * velX + velZ * velZ);
+
+        if (horizontalLen > MAX_SPEED){
+            // skalerer ned
+            double scale = MAX_SPEED / horizontalLen;
+
+            velX = velX * scale;
+            velZ = velZ * scale;
+        }
+        double velY = current.y;
 
         if (mc.options.jumpKey.isPressed()){
-            Vec3d current = vehicle.getVelocity();
-            var velX = current.x * SPEED;
-            var velZ = current.z * SPEED;
-
-
-            vehicle.setVelocity(velX, current.y + VELOCITY_BUMP, velZ);
+            velY =Math.min(current.y + VELOCITY_BUMP, MAX_VELY);
         }
+        vehicle.setVelocity(velX, velY, velZ);
 
-        // set vektor y
+        // Får velocity
+
+
+
+
     }
 }
