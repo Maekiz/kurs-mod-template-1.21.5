1503c6d7ea11a0c91f33aff7a78dbdb8ac287c4b
Maekiz <manoa039@osloskolen.no>
2025-04-28 09:46:03 +0200

    oppdaterte kommentarer

--- src/client/java/kurs/kursmod/mixin/client/FlyBoatMixin.java
+++ src/client/java/kurs/kursmod/mixin/client/FlyBoatMixin.java
@@ -14,16 +14,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class FlyBoatMixin {
-   //private static final Logger LOGGER = LoggerFactory.getLogger("Maekiz");
     private static final double VELOCITY_BUMP = 0.09;
     private static final double MAX_SPEED = 1.5;
     private static final double MAX_VELY = 1.3;
     private static final double SPEED = 1.1;
 
-    // Undeer er en funksjon som kjører hele tiden i minecraft
+    // Setter inn i minecraftClient.class i tick-funksjonen (kjører hele tiden)
     @Inject(at = @At("HEAD"), method = "tick")
     private void repeatingEverySecond(CallbackInfo info) {
-        var mc = MinecraftClient.getInstance();
+        MinecraftClient mc = MinecraftClient.getInstance();
         var player = mc.player;
 
         // sjekke etter spiller:
@@ -36,27 +35,31 @@ public class FlyBoatMixin {
         if (vehicle == null){
             return;
         }
-        // sjekk etter jumpkey/space
+        // Endrer horisontal fart
         Vec3d current = vehicle.getVelocity();
-        var velX = current.x * SPEED;
-        var velZ = current.z * SPEED;
+        double velX = current.x * SPEED;
+        double velZ = current.z * SPEED;
 
         double horizontalLen = Math.sqrt(velX * velX + velZ * velZ);
 
+        // Justerer ned farten hvis lengden til posisjonsvektoren er større en max speed
         if (horizontalLen > MAX_SPEED){
-            // skalerer ned horisontal fart
+
             double scale = MAX_SPEED / horizontalLen;
 
             velX = velX * scale;
             velZ = velZ * scale;
         }
+
         double velY = current.y;
 
+        // sjekke om space/hoppeknappen trykkes
         if (mc.options.jumpKey.isPressed()){
             velY =Math.min(current.y + VELOCITY_BUMP, MAX_VELY);
         }
-        vehicle.setVelocity(velX, velY, velZ);
 
+        // setter ny fart
+        vehicle.setVelocity(velX, velY, velZ);
 
     }
 }
