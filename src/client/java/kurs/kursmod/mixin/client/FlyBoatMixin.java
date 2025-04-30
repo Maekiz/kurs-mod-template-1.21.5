7b9f052513114d64eac9d1f2a880fac6ddf103c7
Maekiz <manoa039@osloskolen.no>
2025-04-30 09:02:42 +0200

    nye kommentarer

--- src/client/java/kurs/kursmod/mixin/client/FlyBoatMixin.java
+++ src/client/java/kurs/kursmod/mixin/client/FlyBoatMixin.java
@@ -12,36 +12,39 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
-public class FlyBoatMixin {
-    private static final double VELOCITY_BUMP = 0.09;
-    private static final double MAX_SPEED = 1.5;
-    private static final double MAX_VELY = 1.3;
-    private static final double SPEED = 1.1;
 
-    // Setter inn i minecraftClient.class i tick-funksjonen (kjører hele tiden)
+// Klasse med hacken
+public class FlyBoatMixin {
+    private static final double VELOCITY_BUMP = 0.09; // variabel for fart oppover
+    private static final double MAX_SPEED = 1.5; // variabel for maks fart
+    private static final double MAX_VELY = 1.3; // begrensning på fart oppover
+    private static final double SPEED = 1.1; // Horisontal fart fremover
+
+    // Setter funksjonen inn i minecraftClient.class i tick-funksjonen (kjører hele tiden)
     @Inject(at = @At("HEAD"), method = "tick")
     private void repeatingEverySecond(CallbackInfo info) {
-        MinecraftClient mc = MinecraftClient.getInstance();
-        var player = mc.player;
+        MinecraftClient mc = MinecraftClient.getInstance(); // variabel for om du er i spillet
+        var player = mc.player; // variebel for spilleren i en verden
 
-        // sjekke etter spiller:
+        // Hvis det ikke er en spiller i en verden
         if (player == null){
-            return;
+            return; // gjør ingenting
         }
-        //sjekker etter båt
+        //variabel for båt
         var vehicle = player.getVehicle();
 
+        // hvis spilleren ikke sitter i en båt
         if (vehicle == null){
-            return;
+            return; // gjør ingenting
         }
-        // Endrer horisontal fart
-        Vec3d current = vehicle.getVelocity();
+        // Endrer horisontal farts-variabler
+        Vec3d current = vehicle.getVelocity(); // Får farten til båten
         double velX = current.x * SPEED;
         double velZ = current.z * SPEED;
 
-        double horizontalLen = Math.sqrt(velX * velX + velZ * velZ);
+        double horizontalLen = Math.sqrt(velX * velX + velZ * velZ); // variabel for fart framover
 
-        // Justerer ned farten hvis lengden til posisjonsvektoren er større en max speed
+        // Justerer ned farten hvis farten framover er større enn maks-farten
         if (horizontalLen > MAX_SPEED){
 
             double scale = MAX_SPEED / horizontalLen;
@@ -50,15 +53,15 @@ public class FlyBoatMixin {
             velZ = velZ * scale;
         }
 
-        double velY = current.y;
+        double velY = current.y; // variabel for oppover fart er farten den allerede har
 
-        // sjekke om space/hoppeknappen trykkes
+        // sjekker om space/hoppeknappen trykkes
         if (mc.options.jumpKey.isPressed()){
-            velY =Math.min(current.y + VELOCITY_BUMP, MAX_VELY);
+            velY =Math.min(current.y + VELOCITY_BUMP, MAX_VELY); // endrer på oppvoverfart
         }
 
         // setter ny fart
-        vehicle.setVelocity(velX, velY, velZ);
+        vehicle.setVelocity(velX, velY, velZ); // setter ny velocity
 
     }
 }
